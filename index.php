ef9ac9cac9bd6f932a42c78ffb2c6e25878e6255
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-01 10:06:49 -0500

    Coffee Maker Object example

--- index.php
+++ index.php
@@ -1,56 +1,9 @@
 <?php
 
-class Team {
-
-    protected $name;
-
-    protected $members = [];
-
-
-    public function __construct($name, $members =[]){
-        $this->name = $name;
-        $this->members = $members;
-    }
-
-    public static function start(...$params){
-        return new static(...$params);
-    }
-
-    public function name(){
-        return $this->name;
-    }
-
-    public function members(){
-        return $this->members;
-    }
-
-    public function add($name){
-        $this->members[] = $name;
-    }
-
-    public function cancel(){
-
+class CoffeeMaker {
+    public function brew(){
+        var_dump('Brewing Coffee');
     }
 }
 
-class Member {
-    protected $name;
-
-    public function __construct($name){
-        $this->name = $name;
-    }
-
-    public function lastViewed(){
-
-    }
-}
-
-
-$acme = Team::start('Acme', [
-    new Member('John Doe'),
-    new Member("Jane Doth")
-]);
-
-$acme->add('Robert Downey Jr.');
-
-var_dump($acme->members());
+(new CoffeeMaker())->brew();
