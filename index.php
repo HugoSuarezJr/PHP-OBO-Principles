ad212819a6eeb7b300d21570d6b407b3d3fe5c6c
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-01 11:23:00 -0500

    Inheritance parent can be overrided by child

--- index.php
+++ index.php
@@ -1,40 +1,28 @@
 <?php
 
-class Collection {
-    protected $items;
+class AchivementType {
+        public function name(){
 
-    public function __construct(array $items)
-    {
-        $this->items = $items;
-    }
+        }
 
-    public function sum($key){
-       return array_sum(array_column($this->items, $key));
-    }
+        public function difficulty(){
+
+        }
+
+        public function icon(){
+
+        }
 }
 
-class VideosCollection extends Collection{
-    public function length(){
-        return $this->sum('length');
+class FirstThousandsPoints extends AchivementType {
+
+    public function qualifier($user){
+
+    }
+
+    // override parent behavior
+
+    public function name(){
+        return "Welcome aboard!";
     }
 }
-
-class Video  {
-    public $title;
-    public $length;
-     
-    public function __construct($title, $length)
-    {
-        $this->title = $title;
-        $this->length = $length;
-    }
-    
-}
-
-$videos = new VideosCollection([
-    new Video('Jaws', 100),
-    new Video('Jaws 2', 200),
-    new Video('Jaws 3', 250)
-]);
-
-echo $videos->length();
