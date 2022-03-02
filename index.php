265fae32d04a59033a508d6cf8853b6f679ff79f
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-01 20:10:07 -0500

    Use inheritance with multiple classes

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
 
-class AchivementType {
-        public function name(){
-
-        }
-
-        public function difficulty(){
-
-        }
-
-        public function icon(){
-
-        }
-}
-
-class FirstThousandsPoints extends AchivementType {
-
-    public function qualifier($user){
-
-    }
-
-    // override parent behavior
-
+class AchievementType {
     public function name(){
-        return "Welcome aboard!";
+        $class = (new ReflectionClass($this))->getShortName();
+
+        return trim(preg_replace('/[A-Z]/', ' $0', $class));
+    }
+
+    public function icon(){
+        return strtolower(str_replace(' ','-', $this->name())).'.png';
     }
 }
+
+class FirstThousandsPoints extends AchievementType {
+        public function qualifier($user){
+
+        }
+}
+
+class FirstBestAnswer extends AchievementType {
+        public function qualifier($user){
+            
+        }
+}
+
+$achievement = new FirstBestAnswer();
+echo $achievement->icon();
