3870e533b715c6b8695b7792312549cb3e29aa26
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-02 17:39:48 -0500

    Use interface/implent keywords to require method signatures for a class to be implemented

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
 
-class CampaignMonitor {
+interface Newsletter{
+    public function subscribe($email);
+}
+
+class CampaignMonitor implements Newsletter{
     public function subscribe($email){
         die("{$email} is subscribed with Campaign Monitor");
     }
 }
 
-class Drip{
+class Drip implements Newsletter{
     public function subscribe($email){
         die("{$email} is subscribed with Drip");
     }
 }
 
 class NewsletterSubscriptionsController {
-    public function store($newsletter){
+    public function store(Newsletter $newsletter){
         $email = 'joe@example.com';
         $newsletter->subscribe($email);
     }
