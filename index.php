f0e4f8673e898dce79947c51993510dfbfce496d
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-02 17:43:38 -0500

    Restructure code

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-
-interface Newsletter{
-    public function subscribe($email);
-}
-
-class CampaignMonitor implements Newsletter{
-    public function subscribe($email){
-        die("{$email} is subscribed with Campaign Monitor");
-    }
-}
-
-class Drip implements Newsletter{
-    public function subscribe($email){
-        die("{$email} is subscribed with Drip");
-    }
-}
-
 class NewsletterSubscriptionsController {
     public function store(Newsletter $newsletter){
         $email = 'joe@example.com';
@@ -23,6 +6,24 @@ class NewsletterSubscriptionsController {
     }
 }
 
+interface Newsletter{
+    public function subscribe($email);
+}
+
+class CampaignMonitor implements Newsletter{
+    public function subscribe($email){
+        var_dump("{$email} is subscribed with Campaign Monitor");
+    }
+}
+
+class Drip implements Newsletter{
+    public function subscribe($email){
+        var_dump("{$email} is subscribed with Drip");
+    }
+}
+
+
 $controller = new NewsletterSubscriptionsController();
 
 $controller->store(new CampaignMonitor());
+$controller->store(new Drip());
