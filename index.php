7d2ba6fe6b53fd05720d0139cddd866cd1a6a8e4
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-03 15:16:23 -0500

    Using a getter to access private variables from an object

--- index.php
+++ index.php
@@ -5,16 +5,23 @@
 
 // Encapsulation => Enclose within a capsule
 class TennisMatch{
+    protected $playerOne;
     public function score(){
         //is there a winner
         // does someone have an advantage
         // are they in deuce
     }
 
+    public function playerOne(){
+        return $this->playerOne;
+        // this allows you to access data of object with protected property. 
+        // must show caution when doing this "Getter".
+    }
+
     protected function hasWinner(){
 
     }
-    private function hasAdvantage(){
+    protected function hasAdvantage(){
         
     }
     protected function inDeuce(){
