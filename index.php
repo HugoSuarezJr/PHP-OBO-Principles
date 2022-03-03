4da1a3ae97e7a7768d4358cdcbdb9de0e1c254e4
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-03 15:37:21 -0500

    Obje ct composition and Abstracts start

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
 <?php
 
-// Notes: * 'private' keyword makes property only usable to class
-// but 'protected' keyword makes property accessible to both class and children of class.
-
-// Encapsulation => Enclose within a capsule
-class TennisMatch{
-    protected $playerOne;
-    public function score(){
-        //is there a winner
-        // does someone have an advantage
-        // are they in deuce
-    }
-
-    public function playerOne(){
-        return $this->playerOne;
-        // this allows you to access data of object with protected property. 
-        // must show caution when doing this "Getter".
-    }
-
-    protected function hasWinner(){
+class Subscription{
+    public function create(){
 
     }
-    protected function hasAdvantage(){
-        
+
+    public function cancel(){
+
     }
-    protected function inDeuce(){
-        
+
+    public function invoice(){
+
+    }
+
+    public function swap($newPlan){
+
     }
 }
-
-$encapsulation = 'Encapsulation';
-
-echo("<h1>$encapsulation allows for you to protect the integrity of an object while also allow to
-send signals to the outside world.</h1>");
