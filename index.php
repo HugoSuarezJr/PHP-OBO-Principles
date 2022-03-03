f5b2ba52587a4153cfa62be1fe78c1378487c2cc
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-03 16:15:49 -0500

    Object Composition for a Subscription class to allow a cancel subscription method using gateway for different servicing providers

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php
 
 class Subscription{
+/**
+ * Option 3: Use Object Compostion
+ * @var \Gateway
+ */
+    protected $gateway;
+
+    public function __construct(Gateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
     public function create(){
 
     }
 
     public function cancel(){
-
+        // How to cancel: api request
+        // find stripe customer
+        $this->gateway->findCustomer();
+        // find stripe subscription by customer
     }
 
     public function invoice(){
@@ -16,4 +30,52 @@ class Subscription{
     public function swap($newPlan){
 
     }
+
+    // Option 1 *Not so good option for this setting*
+    protected function findStripeCustomer(){
+
+    }
+
+    protected function findStripeSubscriptionByCustomer(){
+
+    }
 }
+
+// Option 2 *Use inheritance*
+// class BillableSubscription extends Subscripiton{
+//     protected function findStripeCustomer(){
+
+//     }
+
+//     protected function findStripeSubscriptionByCustomer(){
+
+//     }
+// }
+interface Gateway {
+    public function findCustomer();
+
+    public function findSubscriptionByCustomer();
+}
+class StripeGateway implements Gateway {
+
+    public function findCustomer(){
+
+    }
+
+    public function findSubscriptionByCustomer(){
+
+    }
+}
+
+class BraintreeGateway implements Gateway {
+
+    public function findCustomer(){
+
+    }
+
+    public function findSubscriptionByCustomer(){
+
+    }
+}
+
+new Subcription(new BraintreeGateway());
