60e151da40cd9b5209dc30a02a0649696c0c72c2
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-04 18:49:21 -0500

    When data is connected - give it a proper type.

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
-function register(FirstName $first, LastName $last, Age $age, EmailAddress $email, Password $password){
-    // Be considerate when making value objects. Only use when very clearly is beneificial.
+class Coordinates {
+    public $x;
+    public $y;
+
+    public function __construct($x, $y)
+    {
+        $this->y = $y;
+        $this->x = $x;
+    }
+
+    
 }
+
+function pin(Coordinates $coordinates){
+    $coordinates->x;
+}
+
+function distance(Coordinates $begin, Coordinates $end){
+    
+}
+
+// When data is connected - give it a proper type.
