c413440d2d36e2cef486f16d228a1949cbbc4e6d
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-04 18:38:31 -0500

    Only use value objects when it is clearly beneficial

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
 <?php
 
-// Avoids primative obsession - and readability
-// Helps with consistency
-// Immutability
-
-class Age {
-    private $age;
-
-    public function __construct($age)
-    {
-        if($age < 0 || $age > 120){
-        throw new InvalidArgumentException('That age dont look right');
-    }
-        $this->age = $age;
-    }
-
-    public function increment(){
-        return new self($this->age +1);
-    }
-
-    public function get(){
-        return $this->age;
-    }
+function register(FirstName $first, LastName $last, Age $age, EmailAddress $email, Password $password){
+    // Be considerate when making value objects. Only use when very clearly is beneificial.
 }
-
-$age = new Age(36);
-$age = $age->increment();
-var_dump($age->get());
-
-
-// function register(string $name, Age $age){
-    
-//  return [$age, $name];
-// }
-
-// $age = new Age(35);
-
-// $age->age = -22;
-// var_dump(register("mike", $age));
